2c873258c5a04cd5250032988247fa6fb1e92a6b
Tim <timvan0118@gmail.com>
2022-11-19 13:03:51 -0600

    Setup More

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import tw from "twin.macro";
+import tw from "twin.macro"
 
 export default function App() {
-  return <Body>Hi</Body>;
+  return <H1>T_RACT</H1>
 }
 
-const Body = tw.div``;
+const H1 = tw.div`text-[32rem] text-center my-auto text-blue-400`
